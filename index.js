59c09d77cce752dad63b7b98a0dbacb01c333375
Md. Zahidul Islam <mdzahidulislam.bera@gmail.com>
2024-12-07 01:15:45 +0600

    Delete query for every single  movie

--- index.js
+++ index.js
@@ -90,6 +90,13 @@ async function run() {
             res.send(result)
         })
 
+        app.delete('/movies/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await movieCollection.deleteOne(query);
+            res.send(result)
+        })
+
 
         // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
