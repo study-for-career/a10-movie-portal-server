7d279e61d2c328aa867474577f30a9ec6b5790e1
Md. Zahidul Islam <mdzahidulislam.bera@gmail.com>
2024-12-06 22:18:59 +0600

    Favourite Movie Added to DB

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
 const express = require('express');
 const cors = require('cors')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
 const app = express()
 const port = process.env.PORT || 5000;
 
@@ -8,10 +10,9 @@ app.use(cors())
 app.use(express.json())
 
 
-// BUy5YBTpUONGe247
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.b8foj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-
-const uri = "mongodb+srv://mdzahidulislambera:BUy5YBTpUONGe247@cluster0.b8foj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+console.log(uri)
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -25,10 +26,11 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
         // Send a ping to confirm a successful connection
         const userCollection = client.db('movie_portal_db').collection('userCollection');
         const movieCollection = client.db('movie_portal_db').collection('movieCollection');
+        const favouriteMovies = client.db('movie_portal_db').collection('favouriteMovies');
 
         app.get('/', (req, res) => {
             res.send('Hello Server')
@@ -60,14 +62,18 @@ async function run() {
             res.send(result)
         })
 
-
-
         app.post('/movies', async (req, res) => {
             const movie = req.body;
             const result = await movieCollection.insertOne(movie)
             res.send(result)
         })
-        await client.db("admin").command({ ping: 1 });
+
+        app.post('/favourite_movies', async (req, res) => {
+            const favouriteMovie = req.body;
+            const result = await favouriteMovies.insertOne(favouriteMovie)
+            res.send(result)
+        })
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
